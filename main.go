0b3cb287dbb490a7c99f2bda7279b6e8de7e5606
sxz799 <sxz15371@163.com>
2022-08-27 12:14:16 +0800

    2

--- main.go
+++ main.go
@@ -1,101 +1,181 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
-	"strconv"
-	"time"
-
 	"golang.org/x/sys/windows/registry"
+	"os"
+	"strings"
+	"time"
 )
 
-func getRegQUERY_VALUE(str string) string {
-	tKey, _ := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\`+str, registry.QUERY_VALUE)
-	stringValue, _, _ := tKey.GetStringValue("")
-	defer tKey.Close()
-	return stringValue
+var Key registry.Key
+
+var KeyArray []KeyMap
+
+type KeyMap struct {
+	id     int
+	name   string
+	binary []byte
 }
 
-func delALL(str []string) {
-	tKey, _ := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\`, registry.QWORD)
-	defer tKey.Close()
-	for _, v := range str {
-		delKey(v)
+func queryKeyByBinary(binary []byte) KeyMap {
+	for _, keyMap := range KeyArray {
+		if bytes.Equal(binary, keyMap.binary) {
+			return keyMap
+		}
 	}
+	return KeyMap{}
+}
+func queryKeyByName(name string) KeyMap {
+	for _, keyMap := range KeyArray {
+		if strings.Replace(name, " ", "", -1) == strings.Replace(keyMap.name, " ", "", -1) {
+			return keyMap
+		}
+	}
+	return KeyMap{}
 }
 
-func delKey(str string) bool {
-	tKey, _ := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\`, registry.QWORD)
-	defer tKey.Close()
-	return registry.DeleteKey(tKey, str) == nil
+func InitKeyArray() {
+	KeyArray = append(KeyArray, KeyMap{
+		name:   "L Ctrl",
+		binary: []byte{29, 0},
+	})
+	KeyArray = append(KeyArray, KeyMap{
+		name:   "L Win",
+		binary: []byte{91, 224},
+	})
+	KeyArray = append(KeyArray, KeyMap{
+		name:   "L Alt",
+		binary: []byte{56, 0},
+	})
+	KeyArray = append(KeyArray, KeyMap{
+		name:   "R Alt",
+		binary: []byte{56, 224},
+	})
+	KeyArray = append(KeyArray, KeyMap{
+		name:   "R Win",
+		binary: []byte{92, 224},
+	})
+	KeyArray = append(KeyArray, KeyMap{
+		name:   "R Ctrl",
+		binary: []byte{29, 224},
+	})
 
 }
 
-func showIcons() []string {
-	k, err := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace`, registry.QWORD)
+func showCurrectConfig() {
+	defer func() {
+		a := recover()
+		if a != nil {
+			fmt.Println("error:", a)
+			time.Sleep(time.Second * 10)
+		}
+	}()
+	var err error
+	Key, err = registry.OpenKey(registry.LOCAL_MACHINE, `SYSTEM\CurrentControlSet\Control\Keyboard Layout`, registry.ALL_ACCESS)
 	if err != nil {
-		return nil
+		fmt.Println(err)
+		fmt.Println("注册表打开失败!")
+		return
 	}
-	defer k.Close()
-	keys, _ := k.ReadSubKeyNames(0)
-	return keys
+	value, _, err2 := Key.GetBinaryValue("Scancode Map")
+	if err2 != nil || len(value) < 9 {
+		fmt.Println("未查询当前按键修改")
+	} else if value[8] == 1 {
+		fmt.Println("未查询当前按键修改")
+	} else {
+		value = value[12:]
+		fmt.Println("当前按键修改内容如下")
+		for i := 0; i < len(value); i = i + 4 {
+			key1 := value[i : i+2]
+			key2 := value[i+2 : i+4]
+			if bytes.Equal(key1, key2) {
+				continue
+			}
+			fmt.Println("    ", queryKeyByBinary(key1).name, "  ->  ", queryKeyByBinary(key2).name)
+
+		}
+	}
+}
+func delConfig() bool {
+	err2 := Key.DeleteValue("Scancode Map")
+	if err2 != nil {
+		fmt.Println(err2)
+		fmt.Println("没找到相应键值")
+	}
+	return err2 == nil
+}
+func useConfig(config []byte) bool {
+	err2 := Key.SetBinaryValue("Scancode Map", config)
+	return err2 != nil
+}
+func genRegValue(strs []string) (result []byte) {
+	result = []byte{0, 0, 0, 0, 0, 0, 0, 0}
+	buffer := bytes.NewBuffer([]byte{})
+	binary.Write(buffer, binary.LittleEndian, int32(len(strs)+1))
+	result = append(result, buffer.Bytes()...)
+	for _, str := range strs {
+		split := strings.Split(str, "->")
+		config := append(queryKeyByName(split[0]).binary, queryKeyByName(split[1]).binary...)
+		result = append(result, config...)
+	}
+
+	return
+}
+
+func createConfig() (newConfig []string) {
+	fmt.Println("请根据提示修改配置：")
+	mp := make(map[int]string)
+	mp[1] = "L Ctrl"
+	mp[2] = "L Win"
+	mp[3] = "L Alt"
+	mp[4] = "R Ctrl"
+	mp[5] = "R Win"
+	mp[6] = "R Alt"
+	for i := 1; i <= 6; i++ {
+		fmt.Printf("%d = %s\n", i, mp[i])
+	}
+
+	for i := 1; i <= 6; i++ {
+		fmt.Printf("请输入%s修改后的序号", mp[i])
+		var n int
+		fmt.Scan(&n)
+		if i != n {
+			newConfig = append(newConfig, "  "+mp[i]+"  ->  "+mp[n])
+		}
+	}
+	fmt.Println("你要修改的键位有：")
+	for _, s := range newConfig {
+		fmt.Println(s)
+	}
+	return
 }
 
 func main() {
-	s := showIcons()
-	if len(s) < 1 {
-		fmt.Println("没找到相关磁盘图标！3秒后自动退出")
-		time.Sleep(time.Second * 3)
-		return
-	}
-
-	var input string
-
+	InitKeyArray()
+	showCurrectConfig()
 	for {
-		s := showIcons()
-		if len(s) < 1 {
-			fmt.Println("已经没有可删除的图标了")
+		config := createConfig()
+		fmt.Println("输入Y确认修改,输入D恢复系统默认配置,输入R重新配置,输入其他按键退出...")
+		var n string
+		fmt.Scan(&n)
+		n = strings.ToUpper(n)
+		if n == "Y" {
+			useConfig(genRegValue(config))
 			break
-		}
-		fmt.Println("检测到存在以下图标,请输入相关序号进行删除(输入a可全部删除)：")
-		for i, key_subkey := range s {
-			fmt.Println(i, getRegQUERY_VALUE(key_subkey))
-		}
-		fmt.Println("请输入要删除的图标序号")
-		n, err := fmt.Scan(&input)
-		if n > 1 && err != nil {
-			fmt.Println()
-			fmt.Println()
-			fmt.Println()
-			fmt.Println("输入有误,请重试。")
-			continue
-		}
-		if input == "a" || input == "A" {
-			delALL(s)
+		} else if n == "D" {
+			delConfig()
 			break
-		}
-		id, err2 := strconv.Atoi(input)
-		if err2 != nil {
-			fmt.Println()
-			fmt.Println()
-			fmt.Println()
-			fmt.Println("输入有误,请重试。")
+		} else if n == "R" {
 			continue
+		} else {
+			os.Exit(1)
 		}
 
-		if id > len(s)-1 {
-			fmt.Println()
-			fmt.Println()
-			fmt.Println()
-			fmt.Println("输入有误,请重试。")
-			continue
-		}
-		if delKey(s[id]) {
-			fmt.Println("删除成功！")
-			fmt.Println()
-			fmt.Println()
-			fmt.Println()
-		}
 	}
-	fmt.Println("清理完毕！3秒后自动退出")
-	time.Sleep(time.Second * 3)
+	fmt.Println("修改完毕注销用户重新登录或重启电脑后生效,程序将5秒后退出！")
+	time.Sleep(time.Second * 5)
+
 }
